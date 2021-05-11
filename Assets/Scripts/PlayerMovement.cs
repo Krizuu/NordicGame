8f39040dfcc5b5d2e0913e471e231885ac0daf97
DevBroHere <cezarybujak28@gmail.com>
2021-05-11 20:41:25 +0200

    Android configuration and new walking by joystick

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D body; //Okreœlamy nazwê dla zmiennej przechowuj¹cej model fizyczny postaci
     public Animator animator;
 
+    public Joystick joystick;
+
     float moveLimiter = 0.7f; //zmienna ograniczaj¹ca ruch po ukosie
 
     public float runSpeed = 5.0f;
@@ -22,10 +24,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal"); //Gdy gracz naciœnie przycisk przypisany do metody GetAxisRaw klasy Input (s¹ to strza³ki poziome albo "A", "D") to w przypadku naciœniêcia
-                                                     //strza³ki w lewo albo "A" przypisana zostanie wartoœæ -1 natomiast dla strza³ki w prawo lub "D" bêdzie to wartoœæ 1
-        movement.y = Input.GetAxisRaw("Vertical"); //Analogicznie jak dla poprzedniej funkcji; strza³ka w górê lub "W" to wartoœæ 1 natomiast strza³ka w dó³ lub "S" to wartoœæ -1. 
-                                                 //Brak wykonanej akcji oznacza 0
+        //movement.x = Input.GetAxisRaw("Horizontal"); //Gdy gracz naciœnie przycisk przypisany do metody GetAxisRaw klasy Input (s¹ to strza³ki poziome albo "A", "D") to w przypadku naciœniêcia
+        //strza³ki w lewo albo "A" przypisana zostanie wartoœæ -1 natomiast dla strza³ki w prawo lub "D" bêdzie to wartoœæ 1
+        movement.x = joystick.Horizontal;
+        //movement.y = Input.GetAxisRaw("Vertical"); //Analogicznie jak dla poprzedniej funkcji; strza³ka w górê lub "W" to wartoœæ 1 natomiast strza³ka w dó³ lub "S" to wartoœæ -1. 
+        //Brak wykonanej akcji oznacza 0
+        movement.y = joystick.Vertical;
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
